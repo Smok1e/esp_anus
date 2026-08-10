ffd8137c1777d1684d88b9fc600b8bbdae4db45d
smok1e <fedotcher@gmail.com>
2026-08-10 23:11:09 +0300

    fix typo

--- include/anus/http_interface.hpp
+++ include/anus/http_interface.hpp
@@ -63,7 +63,7 @@ void HttpInterface::registerUri(const char* uri)
 		httpd_resp_set_type(request, "application/json");
 
 #ifdef CONFIG_ANUS_API_ALLOW_CORS
-		httpd_resp_set_hdr(request, "Access-Control-Allos-Origin", "*");
+		httpd_resp_set_hdr(request, "Access-Control-Allow-Origin", "*");
 		httpd_resp_set_hdr(request, "Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 		httpd_resp_set_hdr(request, "Access-Control-Allow-Headers", "Auth, Content-Type, Authentication");
 #endif
